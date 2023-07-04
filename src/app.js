fd178c560401c96426767d783a31cd9051d63e3d
arbeo81 <j.arbeo@gmail.com>
2023-07-04 16:39:39 -0300

    Version corregida con .env

--- src/app.js
+++ src/app.js
@@ -1,18 +1,17 @@
 import express from "express";
 import handlebars from "express-handlebars";
-import Handlebars from "handlebars";
 import mongoose from "mongoose";
-//import dotenv from "dotenv";
-//dotenv.config({path:'.env'});
+import dotenv from "dotenv" 
+dotenv.config();
 import cartsRouter from "./routes/carts.router.js";
 import productsRouter from "./routes/products.router.js";
 import viewsRouter from "./routes/views.router.js";
 import __dirname from "./utils.js";
 
-const port = 8080; 
-//const dbName = process.env.DB_NAME;
-//const dbUser = process.env.DB_USER;
-//const dbPasword = process.env.DB_PASSWORD;
+const port = process.env.PORT || 8080; 
+const dbName = process.env.DB_NAME;
+const dbUser = process.env.DB_USER;
+const dbPasword = process.env.DB_PASSWORD;
 
 const app = express();
 
@@ -32,4 +31,4 @@ app.use("/api/carts", cartsRouter);
 app.use("/api/products", productsRouter);
 app.use("/", viewsRouter);
 
-mongoose.connect("mongodb+srv://jarbeo:Janise3423@cluster0.orjlcud.mongodb.net/PracticaIntegradora")
+mongoose.connect(`mongodb+srv://${dbUser}:${dbPasword}@cluster0.orjlcud.mongodb.net/${dbName}`)
